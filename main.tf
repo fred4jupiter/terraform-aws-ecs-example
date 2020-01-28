f0ef9d3b2cf081b58f23ef2f5f02a1da85d3ef0d
Staehler, Michael <michael.staehler@opitz-consulting.com>
2020-01-28 10:45:09 +0100

    update

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "fredbet-vpc"
+  }
 }
 
 resource "aws_subnet" "public1" {
@@ -31,7 +34,7 @@ resource "aws_subnet" "public2" {
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_subnet" "private1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-central-1a"
@@ -42,44 +45,25 @@ resource "aws_subnet" "private" {
   }
 }
 
+resource "aws_subnet" "private2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "eu-central-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "private-1b"
+  }
+}
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "fredbet-igw"
+  }
 }
 
 resource "aws_route_table" "rt-public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_eip" "nat-eip" {
-}
-
-resource "aws_nat_gateway" "this" {
-  allocation_id = aws_eip.nat-eip.id
-  subnet_id     = aws_subnet.public1.id
-  tags = {
-    Name = "NAT-GW"
-  }
-}
-
-resource "aws_default_route_table" "rt-private" {
-  default_route_table_id = aws_vpc.main.default_route_table_id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.this.id
-  }
-
-  tags = {
-    Name = "private routes"
-  }
-}
-
-resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
@@ -92,24 +76,45 @@ resource "aws_route_table" "public" {
   }
 }
 
-resource "aws_route_table_association" "public" {
+resource "aws_route_table" "rt-private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private routes"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.rt-public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.rt-public.id
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.rt-private.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.rt-private.id
 }
 
 # -------------------------------------------------------------
 
 module "alb" {
   source             = "umotif-public/alb/aws"
-  version            = "~> 1.0"
+  version            = "~> 1.1.0"
   name_prefix        = "alb-fredbet"
   load_balancer_type = "application"
   internal           = false
   vpc_id             = aws_vpc.main.id
   subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
-  tags = {
-    Owner = var.owner
-  }
+
 }
 
 resource "aws_lb_listener" "alb_80" {
@@ -134,7 +139,7 @@ module "ecs-fargate" {
   name_prefix                     = "ecs-fargate"
   vpc_id                          = aws_vpc.main.id
   lb_arn                          = module.alb.arn
-  private_subnet_ids              = [aws_subnet.private.id]
+  private_subnet_ids              = [aws_subnet.private1.id, aws_subnet.private2.id]
   cluster_id                      = aws_ecs_cluster.cluster.id
   task_container_image            = "fred4jupiter/fredbet:latest"
   task_definition_cpu             = 256
@@ -144,8 +149,5 @@ module "ecs-fargate" {
   health_check = {
     port = "traffic-port"
     path = "/"
-  }
-  tags = {
-    Owner = var.owner
   }
 }
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "owner" {
-  description = "owner who created the resources"
-  default     = "mis"
-}
-
-variable "az_count" {
-  description = "Number of used availability zones"
-  default     = 2
-}
