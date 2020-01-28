61501436b32165e76c95d8615573c7976ecae90f
Staehler, Michael <michael.staehler@opitz-consulting.com>
2020-01-28 14:11:42 +0100

    added nat gateway

--- main.tf
+++ main.tf
@@ -63,6 +63,20 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_eip" "nat" {
+  tags {
+    Name = "nat-gw-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public1.id
+  tags {
+    Name = "nat-gw"
+  }
+}
+
 resource "aws_route_table" "rt-public" {
   vpc_id = aws_vpc.main.id
 
@@ -78,6 +92,11 @@ resource "aws_route_table" "rt-public" {
 
 resource "aws_route_table" "rt-private" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
 
   tags = {
     Name = "private routes"
